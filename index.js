f873c8cea4be951b9906099912a53353b4be7099
makatymba2001 <makatymba2001@yandex.ru>
2021-10-22 16:38:44 +0300

    Creating and deleting sections

--- index.js
+++ index.js
@@ -4,12 +4,35 @@ const {Client} = require('pg');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
+const canvas = require('node-canvas');
+const imgur = require('imgur');
+
+imgur.setClientId(process.env.IMGUR_CLIENT_ID);
+imgur.setAPIUrl('https://api.imgur.com/3/');
+
 
 const app = express();
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(express.static(path.join(__dirname, 'static')))
+app.set('views', path.join(__dirname, 'static'))
 app.set('view engine', 'ejs')
+
+app.get(/.css$/, (req, res) => {
+  res.setHeader('Content-Type', 'text/css');
+  res.sendFile(__dirname + '/static' + req.url)
+})
+app.get(/.js$/, (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(__dirname + '/static' + req.url)
+})
+app.get(/.png$/, (req, res) => {
+  res.setHeader('Content-Type', 'image/png');
+  res.sendFile(__dirname + '/static' + req.url);
+})
+app.get(/.svg$/, (req, res) => {
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.sendFile(__dirname + '/static' + req.url);
+})
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
@@ -23,18 +46,24 @@ client.connect();
 app.get('/', (req, res) => {
   res.render('pages/index')
 })
+app.get('/forum', (req, res) => {
+  getAllSectionsData(2, (error, data) => {
+    if (error) res.sendStatus(500);
+    if (data) res.render('pages/forum', {sections_data: data})
+  })
+})
 
 app.post('/database-request', (req, res) => {
   if (req.headers.cookie === process.env.ADMIN_TOKEN) {
-      client.query(req.body.query, (error, result) => {
-        if (error) {
-            res.send({__error__: error.message})
-        }
-        else if (result) {
-            delete result._types;
-            res.send(result);
-        } 
-      })
+    client.query(req.body.query, (error, result) => {
+      if (error) {
+          res.send({__error__: error.message})
+      }
+      else if (result) {
+          delete result._types;
+          res.send(result);
+      } 
+    })
   }
   else{
     res.sendStatus(403)
@@ -49,67 +78,55 @@ app.get('/database', (req, res) => {
   }
 })
 
-app.post('/create-section', (req, res) => {
+app.post('/createSection', (req, res) => {
   let request_body = req.body;
-  let parent_id = request_body.parent_id === null ? null : Number(request_body.parent_id);
-  const request_string = `
-  INSERT INTO Sections (parent_id, title, description, image, childrens) VALUES (${parent_id}, $$${request_body.title}$$, ${request_body.description ? `$$${request_body.description}$$` : null}, $$${request_body.image}$$, '{}') RETURNING *`;
-  if (parent_id){
-    client.query('SELECT id::boolean FROM Sections WHERE id = ' + parent_id + ' LIMIT 1').then(result => {
-      if (result.rows[0]?.id){
+  let parent_id = (request_body.parent_id == 0 || request_body.parent_id === null) ? null : Number(request_body.parent_id);
+  canvas.loadImage(request_body.image_file || request_body.image_link).then(img => {
+    let cnv = canvas.createCanvas(256, 256);
+    let ctx = cnv.getContext('2d');
+    ctx.drawImage(img, 0, 0, 256, 256);
+    let d = cnv.toDataURL();
+    d = d.substring(d.indexOf(',') + 1);
+    if (d.length > 1000000){
+      res.status(400).send({error: 'Image too big'});
+      return;
+    }
+    imgur.uploadBase64(d, null, Date.now().toString())
+    .then(result => {
+      const request_string = `
+  INSERT INTO Sections (parent_id, title, description, image, childrens) VALUES (${parent_id}, $$${request_body.title}$$, ${request_body.description ? `$$${request_body.description}$$` : null}, $$${result.link}$$, '{}') RETURNING *`;
+      if (parent_id){
+        client.query('SELECT id::boolean FROM Sections WHERE id = ' + parent_id + ' LIMIT 1').then(result => {
+          if (result.rows[0]?.id){
+            client.query(request_string, (error, result) => {
+              if (error) res.status(400).send({error: 'Incorrect section data'});
+              if (result) {
+                client.query(`UPDATE Sections SET childrens = array_append(childrens, ${result.rows[0].id}) WHERE id = ${parent_id}`)
+                res.status(200).send(result.rows[0])
+              }
+            })
+          }
+          else{
+            res.status(404).send({error: 'Parent not found'})
+          }
+        })
+        
+      }
+      else if (parent_id === null){
         client.query(request_string, (error, result) => {
           if (error) res.status(400).send({error: 'Incorrect section data'});
-          if (result) {
-            client.query(`UPDATE Sections SET childrens = array_append(childrens, ${result.rows[0].id}) WHERE id = ${parent_id}`)
-            res.status(200).send(result.rows[0])
-          }
+          if (result) res.status(200).send(result.rows[0]);
         })
       }
       else{
-        res.status(404).send({error: 'Parent not found'})
+        res.status(400);
+        return;
       }
     })
-  }
-  else if (parent_id === null){
-    client.query(request_string, (error, result) => {
-      if (error) res.status(400).send({error: 'Incorrect section data'});
-      if (result) res.status(200).send(result.rows[0]);
-    })
-  }
-  else{
-    res.sendStatus(400)
-  }
-})
-app.post('/delete-section', (req, res) => {
-  let request_body = req.body;
-  let parent_id = request_body.parent_id === null ? null : Number(request_body.parent_id);
-  const request_string = `
-  INSERT INTO Sections (parent_id, title, description, image, childrens) VALUES (${parent_id}, $$${request_body.title}$$, ${request_body.description ? `$$${request_body.description}$$` : null}, $$${request_body.image}$$, '{}') RETURNING *`;
-  if (parent_id){
-    client.query('SELECT id::boolean FROM Sections WHERE id = ' + parent_id + ' LIMIT 1').then(result => {
-      if (result.rows[0]?.id){
-        client.query(request_string, (error, result) => {
-          if (error) res.status(400).send({error: 'Incorrect section data'});
-          if (result) {
-            client.query(`UPDATE Sections SET childrens = array_append(childrens, ${result.rows[0].id}) WHERE id = ${parent_id}`)
-            res.status(200).send(result.rows[0])
-          }
-        })
-      }
-      else{
-        res.status(404).send({error: 'Parent not found'})
-      }
-    })
-  }
-  else if (parent_id === null){
-    client.query(request_string, (error, result) => {
-      if (error) res.status(400).send({error: 'Incorrect section data'});
-      if (result) res.status(200).send(result.rows[0]);
-    })
-  }
-  else{
-    res.sendStatus(400)
-  }
+  })
+  .catch(e => {
+    res.status(400).send({error: 'Invalid image'});
+  })
 })
 
 app.get('/getAllSections', (req, res) => {
@@ -121,7 +138,7 @@ app.get('/getAllSections', (req, res) => {
 })
 app.get(/\/getSection/, (req, res) => {
   console.time('get one')
-  let match = req.url.match(/getSection\/([0-9-]+)(\/|$)/);
+  let match = req.url.match(/getSection\/([0-9]+)(\/|$)/);
   if (!match) {
       res.sendStatus(400);
       return;
@@ -131,13 +148,34 @@ app.get(/\/getSection/, (req, res) => {
     res.send(data)
   })
 })
+app.get(/\/deleteSection/, (req, res) => {
+  let match = req.url.match(/deleteSection\/([0-9]+)(\/|$)/);
+  if (!match) {
+      res.sendStatus(400);
+      return;
+  };
+  let id = match[1]
+  Promise.all([
+    client.query('UPDATE Sections SET parent_id = null WHERE parent_id = ' + id),
+    client.query(`UPDATE Sections SET childrens = array_remove(childrens, ${id})`),
+    client.query('DELETE FROM Sections * WHERE id = ' + id),
+  ])
+  .then(results => {
+    if (!results[2].rowCount) {
+      res.sendStatus(404)
+    }
+    else{
+      res.sendStatus(200)
+    }
+  })
+})
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // ------------------------------------------------------
 
 function getAllSectionsData(iterations_count = 2, callback){
-  client.query('SELECT * FROM Sections WHERE parent_id IS NULL').then(result => {
+  client.query('SELECT * FROM Sections WHERE parent_id IS NULL ORDER BY id').then(result => {
     sectionChildrenIterator(result.rows, iterations_count, callback)
   })
 }
@@ -163,7 +201,7 @@ function sectionChildrenIterator(data, iteration_count = 1, callback){
   }
   let ids = [];
   const iterator = () => {
-    if (iteration_count-- && cycle_data.length){
+    if (iteration_count-- > 0 && cycle_data.length){
       ids = [];
       cycle_data.forEach(section => {
           ids = ids.concat(section.childrens);
